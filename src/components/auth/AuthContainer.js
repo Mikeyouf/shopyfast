76752e2671a3c160a70209229d924bc833256760
Mikeyouf <mik_anarchy@hotmail.com>
2024-06-07 18:35:12 +0200

    modif bug conenxion

--- src/components/auth/AuthContainer.js
+++ src/components/auth/AuthContainer.js
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import { auth } from "../../config/firebase"; // Assurez-vous d'importer auth depuis votre configuration Firebase
-import { Button, Typography } from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
 import GoogleIcon from "@mui/icons-material/Google";
-import EmailIcon from '@mui/icons-material/Email';
+import { Button, Typography } from "@mui/material";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { child, get, getDatabase, ref, set, update } from "firebase/database";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../config/firebase";
 import Center from "../utils/Center";
 import EmailPasswordAuthModal from "./EmailPasswordAuthModal";
-import { getDatabase, ref, set } from "firebase/database";
 
 const AuthContainer = (props) => {
   const navigate = useNavigate();
@@ -17,37 +17,52 @@ const AuthContainer = (props) => {
 
   const signInWithGoogle = () => {
     setDisabled(true);
-
-    // Créez une nouvelle instance du fournisseur Google et définissez les paramètres personnalisés
     const provider = new GoogleAuthProvider();
-    provider.setCustomParameters({
-      prompt: 'select_account'
-    });
+    provider.setCustomParameters({ prompt: "select_account" });
 
     signInWithPopup(auth, provider)
       .then((result) => {
         const uid = result.user.uid;
         const db = getDatabase();
-        console.log(result);
-        // Vérifiez si l'utilisateur existe déjà dans la base de données ou créez une nouvelle entrée
-        console.log('Setting user data in database', uid);
-        set(ref(db, 'users/' + uid), {
-          email: result.user.email,
-          shoppingLists: {},
-          // autres informations
-        })
-        // console.log("db : ", db)
-        .then(() => console.log('User data set in database'))
-        .catch(error => console.error('Error setting user data in database', error));
+        const userRef = ref(db, "users/" + uid);
+
+        get(child(ref(db), "users/" + uid))
+          .then((snapshot) => {
+            if (snapshot.exists()) {
+              // L'utilisateur existe déjà, mettez à jour les informations nécessaires
+              update(userRef, {
+                email: result.user.email,
+                // autres informations à mettre à jour
+              })
+                .then(() => console.log("User data updated in database"))
+                .catch((error) =>
+                  console.error("Error updating user data in database", error)
+                );
+            } else {
+              // L'utilisateur n'existe pas, créez une nouvelle entrée
+              set(userRef, {
+                email: result.user.email,
+                shoppingLists: {},
+                // autres informations
+              })
+                .then(() => console.log("User data set in database"))
+                .catch((error) =>
+                  console.error("Error setting user data in database", error)
+                );
+            }
+          })
+          .catch((error) =>
+            console.error("Error checking user data in database", error)
+          );
 
         setDisabled(false);
-        navigate("/"); // Naviguez vers l'écran authentifié
+        navigate("/");
       })
       .catch((error) => {
         setErrorMessage(error.code + ": " + error.message);
         setDisabled(false);
       });
-  };    
+  };
 
   return (
     <Center height={"auto"}>
@@ -57,10 +72,7 @@ const AuthContainer = (props) => {
         disabled={disabled}
         variant="contained"
         onClick={signInWithGoogle}
-        sx={{
-          width: '100%',
-          margin: '8px',
-        }}
+        sx={{ width: "100%", margin: "8px" }}
       >
         Google
       </Button>
@@ -71,12 +83,10 @@ const AuthContainer = (props) => {
         onClick={() => setModalOpen(true)}
         disabled={disabled}
         sx={{
-          backgroundColor: '#4CAF50',
-          '&:hover': {
-            backgroundColor: '#43A047', 
-          },
-          width: '100%', 
-          margin: '8px',
+          backgroundColor: "#4CAF50",
+          "&:hover": { backgroundColor: "#43A047" },
+          width: "100%",
+          margin: "8px",
         }}
       >
         Email
@@ -84,7 +94,10 @@ const AuthContainer = (props) => {
       <Typography sx={{ mt: 2 }} color={"red"}>
         {errorMessage}
       </Typography>
-      <EmailPasswordAuthModal open={modalOpen} handleClose={() => setModalOpen(false)} />
+      <EmailPasswordAuthModal
+        open={modalOpen}
+        handleClose={() => setModalOpen(false)}
+      />
     </Center>
   );
 };
